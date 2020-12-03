7b2e86e946dd3a52c354581aa0f143953ef5c513
Insomnia <insofan3156@gmail.com>
2020-12-03 20:49:34 +0800

    add: flutter

---
+++ ios/Todo/Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Insomnia on 2020/12/3.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
