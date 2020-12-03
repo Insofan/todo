bc35b6c7ae66ec0e561197cf2306566ebbbdd2d4
Insomnia <insofan3156@gmail.com>
2020-12-03 23:52:36 +0800

    feat: proto intergeration

--- server/src/main/java/com/todo/server/ServerApplication.java
+++ server/src/main/java/com/todo/server/ServerApplication.java
@@ -2,13 +2,12 @@ package com.todo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 
 }
